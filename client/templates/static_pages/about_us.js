9e96b512a0fdc74a21c8339e65a5d33b3b6f5399
tyler dunkel <tdunkel@iwu.edu>
2015-03-13 21:19:52 -0500

    jquery waypoint animations

--- client/templates/static_pages/about_us.js
+++ client/templates/static_pages/about_us.js
@@ -1,11 +1,40 @@
-Template.homeWhatSection.rendered = function() {
-	$('.notify').waypoint(function(direction) {
-            alert('Top of notify element hit top of viewport.');
-            $('#dash-image').addClass('animate');
-            $('#dash-image').addClass('fadeInDown');
-            //this.destroy();
-        },
-        {
-            offset: '90%'
-    });
+Template.carousel.rendered = function() {
+	$(window).load(function() {
+		$('.notify').waypoint(function(direction) {
+				if (direction === "down") {
+		            //console.log("waypoint fire");
+		            $('#dash-image').addClass('animate');
+		            $('#dash-image').addClass('fadeInLeft');
+		            $('#dash-image').addClass('animated');
+		        }
+	        },
+	        {
+	            offset: '80%',
+	            triggerOnce: true
+	    });
+
+	    $('.home-what-icons').waypoint(function(direction) {
+	    	if (direction === "down") {
+		    	$('.home-what-icons').addClass('animate');
+		    	$('.home-what-icons').addClass('fadeInUp');
+		    	$('.home-what-icons').addClass('animated');
+		    }
+	    },
+	    {
+	    	offset: '80%',
+	    	triggerOnce: true
+	    });
+
+	    $('.dashboard-features').waypoint(function(direction) {
+	    	if (direction === "down") {
+	    		$('.dashboard-features').addClass('animate');
+	    		$('.dashboard-features').addClass('fadeInRight');
+		    	$('.dashboard-features').addClass('animated');
+	    	}
+	    },
+	    {
+	    	offset: '50%',
+	    	triggerOnce: true
+	    });
+	});
 }
