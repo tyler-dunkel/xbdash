fdfdf6cb4139ba52a892ecde4bebed52922eeae1
tyler dunkel <tdunkel@iwu.edu>
2015-03-17 20:59:13 -0500

    added schema package and added a schema

---
+++ server/xboxApi/xbox_api_queue.js
@@ -0,0 +1,17 @@
+var xboxApiQueue = [];
+var callInQueue;	//current api call in the queue. will be the url and a callback
+Tracker.autorun(function() {
+	if (xboxApiQueue.length) {
+		callInQueue = xboxApiQueue.shift();
+		Meteor._debug(callInQueue);
+	}
+	else { Meteor._debug("fuck damn it"); }
+});
+
+Meteor.methods({
+	testMethod: function() {
+		xboxApiQueue.push("this is whats in the queue");
+		Meteor._debug("this fired");
+		//Meteor._debug(xboxApiQueue[0]);
+	}
+})
