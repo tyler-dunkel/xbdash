0082189d7d036b83029a151ff443f9c076480f4c
kg187 <kguirao87@gmail.com>
2015-03-28 17:47:32 -0400

    starting job queue implementation

--- server/startup.js
+++ server/startup.js
@@ -1,4 +1,33 @@
 //server
+Meteor.startup(function() {
+	Meteor._debug('started debug');
+	jc = JobCollection('xbdJobCollection');
+	xbdqueue = jc.processJobs(
+		'checkGamertag', {
+			concurrency: 1,
+			payload: 1,
+			pollInterval: 1000,
+			perfetch: 1
+		},
+		function (job, callback) {
+			Meteor._debug('started worker');
+			job.done();
+			callback();
+		}
+	);
+
+	var job = new Job(
+		jc,
+		'checkGamertag', {
+			message: 'checked it again'
+		}
+	);
+
+	job.save();
+
+	//jc.getJob();
+});
+
 process.env.MAIL_URL="smtp://xboxdashbugreporter%40gmail.com:theskyisblue@smtp.gmail.com:465/";
 Meteor.methods({
 	contactUsEmail: function(name, email, subject, text) {
@@ -10,12 +39,3 @@ Meteor.methods({
 		});
 	}
 });
-
-// Meteor.startup(function() {
-// 	Email.send({
-// 		from: "xboxdashbugreporter@gmail.com",
-// 		to: "kguirao87@gmail.com",
-// 		subject: "hello this is an email from xboxdash",
-// 		text: "does this work?"
-// 	});
-// });
