c651ae0ccabd2a0ba9fdba9f8882b231dafdd19f
tyler dunkel <tdunkel@iwu.edu>
2015-04-08 17:42:45 -0500

    clean up comment out things

--- server/startup.js
+++ server/startup.js
@@ -1,39 +1,6 @@
 //server
 Meteor.startup(function() {
-	Meteor._debug('started debug');
-	jc = JobCollection('xbdJobCollection');
-	jc.startJobServer();
-	//jc.setLogStream(process.stdout);
-	var xbdqueue = jc.processJobs(
-		'testJob', {
-			pollInterval: 1000
-		},
-		function (job, callback) {
-			Meteor._debug(job.data.message);
-			job.done();
-			callback();
-		}
-	);
-	//xbdqueue.resume();
-	//jc.startJobServer();
-
-	var job = new Job(
-		jc,
-		'testJob', {
-			message: 'checked it again'
-		}
-	);
-
-	job.save({}, function(error, result) {
-		if (error) Meteor._debug(error);
-		else {
-			//Meteor._debug(result);
-			//var qll = xbdqueue.length();
-			//Meteor._debug(qll);
-		}
-	});
-
-	//jc.getJob();
+	
 });
 
 process.env.MAIL_URL="smtp://xboxdashbugreporter%40gmail.com:theskyisblue@smtp.gmail.com:465/";
--- server/xboxApi/xbox_api_queue.js
+++ server/xboxApi/xbox_api_queue.js
@@ -1,12 +1,12 @@
 var xboxApiQueue = [];
 var callInQueue;	//current api call in the queue. will be the url and a callback
-Tracker.autorun(function() {
-	if (xboxApiQueue.length) {
-		callInQueue = xboxApiQueue.shift();
-		Meteor._debug(callInQueue);
-	}
-	else { Meteor._debug("fuck damn it"); }
-});
+// Tracker.autorun(function() {
+// 	if (xboxApiQueue.length) {
+// 		callInQueue = xboxApiQueue.shift();
+// 		Meteor._debug(callInQueue);
+// 	}
+// 	else { Meteor._debug("fuck damn it"); }
+// });
 
 Meteor.methods({
 	testMethod: function() {
