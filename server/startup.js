8d9a4737fe9d578efc000f4eada84311b344bc6d
kg187 <kguirao87@gmail.com>
2015-04-29 22:59:53 -0400

    fix logout layouts for log in and sign up pages

--- server/startup.js
+++ server/startup.js
@@ -1,6 +1,23 @@
 //server
 Meteor.startup(function() {
-	
+
+});
+
+function updateUserData(userId) {
+	Meteor._debug(userId);
+	Meteor._debug("this is the updateUserData fucntion");
+	var user = Meteor.users.findOne(userId);
+	Meteor._debug(user);
+	if (typeof user.profile.xuid !== 'undefined') {
+		Meteor._debug("fires when xuid is present");
+	} else {return;}
+}
+
+UserStatus.events.on("connectionLogin", function(fields) {
+	Meteor._debug("this is the connectionActive function");
+	var updateUserDataTimer = Meteor.setInterval(function() {
+		updateUserData(fields.userId);
+	}, 5000);
 });
 
 process.env.MAIL_URL="smtp://xboxdashbugreporter%40gmail.com:theskyisblue@smtp.gmail.com:465/";
