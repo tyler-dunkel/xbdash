b247d1b27b94a2b8b5a895d5e1bd853497d4b43c
kg187 <kguirao87@gmail.com>
2015-05-07 23:33:56 -0400

    started working on different features

---
+++ client/helpers.js
@@ -0,0 +1,35 @@
+Template.registerHelper({
+    appName: function () {
+        return "XboxDash";
+    },
+    chkEmail: function () {
+        //var userId = Meteor.userId();
+        console.log(Meteor.user().services);
+        //console.log(Meteor.user().services.facebook.email);
+        if (!Meteor.user().emails) {
+            if (Meteor.user().services.twitter.screenName) {
+                return true;
+            }
+        }
+        return false;
+    },
+    chkSocial: function () {
+        if (!Meteor.user().services.twitter || 
+            !Meteor.user().services.facebook) {
+            return true;
+        }
+        return false;
+    },
+    chkFacebook: function () {
+        if (Meteor.user().services.facebook) {
+            return true;
+        }
+        return false;
+    },
+    chkTwitter: function () {
+        if (Meteor.user().services.twitter) {
+            return true;
+        }
+        return false;
+    }
+});
