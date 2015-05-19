fb5cbafb14d9a7c4b00d3078fd14383d7b2a95a4
kg187 <kguirao87@gmail.com>
2015-05-19 14:21:01 -0400

    fixing achievement graphing

--- client/templates/dashboard_pages/chart_achievements.js
+++ client/templates/dashboard_pages/chart_achievements.js
@@ -1,22 +1,27 @@
 Template.achievementsChart.rendered = function() {
-	var margin = {top: 10, right: 25, bottom: 10, left: 25},
+	var margin = {top: 0, right: 0, bottom: 15, left: 25},
 		width = $(".chart-wrapper").width(),
-		height = 240;
+		height = 300;
 
+	// defines x as time scale
 	var x = d3.time.scale()
-			.range([0, width - 25]);
+			.range([0, width - margin.left - margin.right]);
 
 	var y = d3.scale.linear()
-			.range([height - 30, 0]);
+			.range([height - margin.bottom, 0]);
 
+	// defines axes
 	var xAxis = d3.svg.axis()
 				.scale(x)
-				.orient("bottom");
+				.orient("bottom")
+				.ticks(7);
 
 	var yAxis = d3.svg.axis()
 				.scale(y)
-				.orient("left");
+				.orient("left")
+				.ticks(7);
 
+	// defines tick values
 	var line = d3.svg.line()
 				.x(function(d) {
 					return x(d.progression);
@@ -35,7 +40,7 @@ Template.achievementsChart.rendered = function() {
 
 	svg.append("g")
 		.attr("class", "x axis")
-		.attr("transform", "translate(0," + 200 + ")");
+		.attr("transform", "translate(0," + 279 + ")");
 
 	svg.append("g")
 		.attr("class", "y axis")
@@ -51,7 +56,7 @@ Template.achievementsChart.rendered = function() {
 		var userAchievementsDataSet = userAchievements.find({ userId: userId, progressState: true }, { sort: { progression: -1 }, limit: 50 }).fetch();
 		//var dataset = Points.find({}, { sort: { date: -1 } }).fetch();
 
-		 var paths = svg.selectAll('path')
+		var paths = svg.selectAll('path')
 		 			.data([userAchievementsDataSet]);
 
 		x.domain(d3.extent(userAchievementsDataSet, function(d) {
@@ -92,7 +97,39 @@ Template.achievementsChart.rendered = function() {
 					.exit()
 					.remove();
 	});
+
+	function resize() {
+		/* Find the new window dimensions */
+		var margin = {top: 0, right: 0, bottom: 15, left: 25},
+		width = parseInt(d3.select("#achievementsChart").style("width")) - margin.left,
+		height = parseInt(d3.select("#achievementsChart").style("height")) - margin.bottom;
+
+		x.range([0, width - margin.left]);
+		y.range([height - margin.bottom, 0]);
+
+		svg
+			.attr("width", width + margin*2)
+			.attr("height", height + margin*2)
+
+		svg
+			.select(".x.axis")
+			.transition()
+			.duration(500)
+			.call(xAxis);
+
+		svg
+			.select(".y.axis")
+			.transition()
+			.duration(500)
+			.call(yAxis);
+
+		svg.selectAll('path')
+		 	.data([userAchievementsDataSet])
+			.attr("d", line);
+	}
+	d3.select(window).on('resize', resize);
 }
 
 Tracker.autorun(function() {
+	resize();
 });
