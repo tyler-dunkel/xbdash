e2fffb7029a1330e4d33e52e229f373e1a40507d
tyler dunkel <tdunkel@iwu.edu>
2015-06-01 17:10:33 -0500

    fixed nav stuff

--- client/templates/dashboard_pages/dashboard.js
+++ client/templates/dashboard_pages/dashboard.js
@@ -5,42 +5,42 @@ Template.dashboard.rendered = function() {
 }
 
 Template.dashboard.events({
-    'click #logout': function(event) {
-        Meteor.logout(function(error){
-            if (error) {
-                throw new Meteor.Error("Logout failed");
-            } else {
-            	Router.go('home');
-				return;
-            }
-        })
-    },
-    'click [ui-toggle]': function(event) {
-        event.preventDefault();
-        var $this = $(event.target);
-        $this.attr('ui-toggle') || ($this = $this.closest('[ui-toggle]'));
+    // 'click #logout': function(event) {
+    //     Meteor.logout(function(error){
+    //         if (error) {
+    //             throw new Meteor.Error("Logout failed");
+    //         } else {
+    //         	Router.go('home');
+				// return;
+    //         }
+    //     })
+    // },
+    // 'click [ui-toggle]': function(event) {
+    //     event.preventDefault();
+    //     var $this = $(event.target);
+    //     $this.attr('ui-toggle') || ($this = $this.closest('[ui-toggle]'));
         
-        $this.toggleClass('active'); // toggles active class on dedent/indent
+    //     $this.toggleClass('active'); // toggles active class on dedent/indent
         
-        var $target = $($this.attr('target')) || $this;
-        $target.toggleClass($this.attr('ui-toggle'));
-        console.log("this was the clicking happening");
-        resizeAchievementChart();
-        resizeGamerscoreChart();
-        resizeGamesChart();
-    },
-    'click [ui-nav] a': function(event) {
-        var $this = $(event.target), $active;
-        $this.is('a') || ($this = $this.closest('a'));
+    //     var $target = $($this.attr('target')) || $this;
+    //     $target.toggleClass($this.attr('ui-toggle'));
+    //     console.log("this was the clicking happening");
+    //     resizeAchievementChart();
+    //     resizeGamerscoreChart();
+    //     resizeGamesChart();
+    // },
+    // 'click [ui-nav] a': function(event) {
+    //     var $this = $(event.target), $active;
+    //     $this.is('a') || ($this = $this.closest('a'));
         
-        $active = $this.parent().siblings( ".active" );
-        $active && $active.toggleClass('active').find('> ul:visible').slideUp(200);
+    //     $active = $this.parent().siblings( ".active" );
+    //     $active && $active.toggleClass('active').find('> ul:visible').slideUp(200);
         
-        ($this.parent().hasClass('active') && $this.next().slideUp(200)) || $this.next().slideDown(200);
-        $this.parent().toggleClass('active');
+    //     ($this.parent().hasClass('active') && $this.next().slideUp(200)) || $this.next().slideDown(200);
+    //     $this.parent().toggleClass('active');
         
-        $this.next().is('ul') && event.preventDefault();
-    }
+    //     $this.next().is('ul') && event.preventDefault();
+    // }
 });
 
 Template.dashboardApp.helpers({
---
+++ client/templates/dashboard_pages/dashboard_header.js
@@ -0,0 +1,41 @@
+Template.dashboardHeader.events({
+	'click #logout': function(event) {
+        Meteor.logout(function(error){
+            if (error) {
+                throw new Meteor.Error("Logout failed");
+            } else {
+            	Router.go('home');
+				return;
+            }
+        })
+    },
+    'click [ui-toggle]': function(event) {
+        event.preventDefault();
+        var $this = $(event.target);
+        $this.attr('ui-toggle') || ($this = $this.closest('[ui-toggle]'));
+        
+        $this.toggleClass('active'); // toggles active class on dedent/indent
+        
+        var $target = $($this.attr('target')) || $this;
+        $target.toggleClass($this.attr('ui-toggle'));
+        var currentRoute = Router.current().route.getName();
+        console.log(currentRoute);
+        if (currentRoute === 'home') {
+	        resizeAchievementChart();
+	        resizeGamerscoreChart();
+	        resizeGamesChart();
+	    }
+    },
+    'click [ui-nav] a': function(event) {
+        var $this = $(event.target), $active;
+        $this.is('a') || ($this = $this.closest('a'));
+        
+        $active = $this.parent().siblings( ".active" );
+        $active && $active.toggleClass('active').find('> ul:visible').slideUp(200);
+        
+        ($this.parent().hasClass('active') && $this.next().slideUp(200)) || $this.next().slideDown(200);
+        $this.parent().toggleClass('active');
+        
+        $this.next().is('ul') && event.preventDefault();
+    }
+});
