6536255efd5b7b777cd955b72e75d0e80656e456
kg187 <kguirao87@gmail.com>
2015-06-29 18:57:52 -0400

    fixed games UX and template seperation

--- client/templates/games_pages/game_template.js
+++ client/templates/games_pages/game_template.js
@@ -12,10 +12,10 @@ Template.gameRating.rendered = function() {
     });
 }
 
-Template.gameTemplate.helpers({
+Template.myTopGamesTemplate.helpers({
     dateFormat: function() {
-		return moment(this.gameReleaseDate).format('l');
-	},
+        return moment(this.gameReleaseDate).format('l');
+    },
     gamesImage: function () {
         console.log(this);
         var xbdGame = xbdGames.findOne({ _id: this.gameId });
@@ -37,4 +37,30 @@ Template.gameTemplate.helpers({
         }
         return image;
     }
+});
+Template.gamesReleaseDateTemplate.helpers({
+    dateFormat: function() {
+        return moment(this.gameReleaseDate).format('l');
+    },
+    gamesImage: function () {
+        console.log(this);
+        var xbdGame = xbdGames.findOne({ _id: this.gameId });
+        var gameDetail = gameDetails.findOne({ gameId: this.gameId });
+        var image = "/img/xboxdash_greenicon.png";
+        if (xbdGame.platform === 'Xenon') {
+            gameDetail.gameArt.forEach(function(art) {
+                if (art.Purpose === 'BoxArt' && art.Width === 219) {
+                    image =  art.Url;
+                }
+            });
+        }
+        if (xbdGame.platform === 'Durango') {
+            gameDetail.gameArt.forEach(function(art) {
+                if (art.Purpose === 'BrandedKeyArt' && art.Width === 584) {
+                    image =  art.Url;
+                }
+            });
+        }
+        return image;
+    },
 });
