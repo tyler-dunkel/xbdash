6631d9e56c61d74bc3c7f8e045a439006e37882d
kg187 <kguirao87@gmail.com>
2015-07-06 18:24:12 -0400

    working on game singles page

--- client/templates/games_pages/games_single.js
+++ client/templates/games_pages/games_single.js
@@ -8,11 +8,75 @@ Template.gamesSinglePage.helpers({
 		var slug = Router.current().params.slug;
 		var game = xbdGames.findOne({ slug: slug });
 		return gameDetails.findOne({ gameId: game._id });
-	}
+	},
+	gamesImage: function () {
+        //var xbdGame = xbdGames.findOne({ _id: this.gameId });
+        var game = xbdGames.findOne({ _id: this.gameId });
+        var image = "/img/xboxdash_greenicon.png";
+        if (game.platform === 'Xenon') {
+            this.gameArt.forEach(function(art) {
+                if (art.Purpose === 'BoxArt' && art.Width === 219) {
+                    image =  art.Url;
+                }
+            });
+        }
+        if (game.platform === 'Durango') {
+            this.gameArt.forEach(function(art) {
+                if (art.Purpose === 'BrandedKeyArt' && art.Width === 584) {
+                    image =  art.Url;
+                }
+            });
+        }
+        return image;
+    },
+    dateFormat: function () {
+        return moment(this.gameReleaseDate).format('l');
+    },
+    gamePlatform: function () {
+    	var game = xbdGames.findOne({ _id: this.gameId });
+    	return game.platform;
+    },
+    achievementsList: function () {
+        return xbdAchievements.find({ gameId: this.gameId }, { sort: { value: 1, userPercentage: -1 }, limit: 100 });
+    },
+    achievementImage: function () {
+        var xbdGame = xbdGames.findOne({ _id: this.gameId });
+        var gameDetail = gameDetails.findOne({ gameId: this.gameId });
+        var image = "/img/xboxdash_greenicon.png";
+        if (xbdGame.platform === 'Xenon') {
+            gameDetail.gameArt.forEach(function(art) {
+                if (art.Purpose === 'BoxArt') {
+                    image =  art.Url;
+                }
+            });
+        }
+        if (xbdGame.platform === 'Durango') {
+            gameDetail.gameArt.forEach(function(art) {
+                if (art.Purpose === 'BrandedKeyArt') {
+                    image =  art.Url;
+                }
+            });
+        }
+        return image;
+    }
 });
 
 Template.gamesSinglePage.events({
 });
+
+Template.gameRatingSingle.rendered = function() {
+    $('.game-rating').raty({
+        readOnly: true,
+        numberMax : 5,
+        path: '/img',
+        score: function() {
+            return $(this).attr('data-score');
+        },
+        starHalf: 'star-half.png',
+        starOff: 'star-off.png',
+        starOn: 'star-on.png'
+    });
+}
 
 Tracker.autorun(function() {
 });
