01696b9a8db6e157be6fe838c60a7cfe2a942ec6
kg187 <kguirao87@gmail.com>
2015-07-30 19:41:41 -0400

    finished help page

--- client/templates/help_pages/help.js
+++ client/templates/help_pages/help.js
@@ -1,4 +1,23 @@
+Template.helpPage.created = function() {
+}
+
 Template.helpPage.rendered = function() {
+	$('#sidebar').affix({
+	    offset: {     
+	      top: $('#sidebar').offset().top,
+	      bottom: ($('footer').outerHeight(true) + $('.application').outerHeight(true)) + 40
+	    }
+	});
+
+	$(document).on('show','.accordion', function (e) {
+	     //$('.accordion-heading i').toggleClass(' ');
+	     $(e.target).prev('.accordion-heading').addClass('accordion-opened');
+	});
+
+	$(document).on('hide','.accordion', function (e) {
+	    $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
+	    //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
+	});
 }
 
 Template.helpPage.events({
