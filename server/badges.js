7a96767bc706e8133ee401e1d03e2f4f7bb99f81
tyler dunkel <tdunkel@iwu.edu>
2015-08-04 10:36:51 -0500

    deleted badge stuff

--- server/badges.js
+++ server/badges.js
@@ -1,11 +1,11 @@
-Meteor.setInterval(function() {
-	var users = Meteor.users.find({"status.online": true});
-	if (!users.count()) {
-		Meteor._debug("there is no one signed up");
-		return;
-	}
-	users.forEach(function(user) {
-		Meteor._debug("user ID is: " + user._id);
-	});
+// Meteor.setInterval(function() {
+// 	var users = Meteor.users.find({"status.online": true});
+// 	if (!users.count()) {
+// 		Meteor._debug("there is no one signed up");
+// 		return;
+// 	}
+// 	users.forEach(function(user) {
+// 		Meteor._debug("user ID is: " + user._id);
+// 	});
 
-}, 300000);
+// }, 300000);
