bee3d0c7b7532c94ca411911916695c53141482d
tyler dunkel <tdunkel@iwu.edu>
2015-08-10 10:46:23 -0500

    search bar overlay stuff

--- client/templates/search_pages/search.js
+++ client/templates/search_pages/search.js
@@ -1,8 +1,43 @@
+var overlayShow = new ReactiveVar();
+
+var overlay = {
+	show: function() {
+		overlayShow.set('show');
+	},
+	hide: function() {
+		overlayShow.set('hide');
+	}
+}
+
 Template.searchBar.rendered = function() {
 }
 
 Template.searchBar.events({
+	"click .navbar-search .form-control": function (e) {
+		console.log("clicked search bar");
+		overlay.show();
+	}
 });
 
 Tracker.autorun(function() {
+});
+
+Template.searchOverlayApp.rendered = function() {
+	$("#search-input").focus();
+}
+
+Template.searchOverlay.events({
+	"click .overlay-close": function(e) {
+		console.log("clicked close");
+		overlay.hide();
+	}
+})
+
+Template.searchOverlay.helpers({
+	overlayVisible: function() {
+		console.log("search overlay firing");
+		if (overlayShow.get() === 'show') {
+			return true;
+		}
+	}
 });
