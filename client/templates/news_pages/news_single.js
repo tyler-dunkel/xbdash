8c15d8e5942f459b06a031f940741fcd5341faa9
kg187 <kguirao87@gmail.com>
2015-09-19 18:19:15 -0400

    working on leaderboards api

--- client/templates/news_pages/news_single.js
+++ client/templates/news_pages/news_single.js
@@ -3,15 +3,15 @@ Template.newsSinglePage.rendered = function() {
 
 Template.newsSinglePage.helpers({
 	article: function() {
-		var slug = Router.current().params.id;
-		return xbdNews.findOne({ slug: id });
+		var slug = Router.current().params.slug;
+		return xbdNews.findOne({ slug: slug });
 	},
 	published: function () {
 		return moment(this.published).format('MMMM Do YYYY, h:mm a');
 	},
 	shareData: function () {
-		var slug = Router.current().params.id;
-		var article = xbdNews.findOne({ slug: id });
+		var slug = Router.current().params.slug;
+		var article = xbdNews.findOne({ slug: slug });
 	    return {
 	    	title: article.title,
 	    	author: article.author
