38653193b020696c1d21de6f73c010a26d0ff701
kg187 <kguirao87@gmail.com>
2015-12-21 00:34:15 -0500

    added missing functions/cron jobs, resynced all files to work with percolate cron package

---
+++ server/leaderboards_page/leaderboards_cron.js
@@ -0,0 +1,59 @@
+SyncedCron.add({
+	name: "non-optomistic update user leaderboard ranks",
+	schedule: function(parser) {
+		return parser.text('every 20 min');
+	},
+	job: function() {
+		leaderboardsApi.updateUserRanks();
+	}
+});
+
+SyncedCron.add({
+	name: "optomistic update user leaderboard ranks",
+	schedule: function(parser) {
+		return parser.text('every 5 min');
+	},
+	job: function() {
+		var onlineUsers = Meteor.users.find({"status.online": true});
+		if (onlineUsers.count() < 1) {
+			return;
+		}
+		onlineUsers.forEach(function(user) {
+			leaderboardsApi.updateUserRanks();
+		});
+	}
+});
+
+SyncedCron.add({
+	name: "non-optimistic update user leaderboard counts",
+	schedule: function(parser) {
+		return parser.text('every 2 hours');
+	},
+	job: function() {
+		var users = meteor.users.find({$or: [{'gamertagScanned.status': 'true'}, {'gamertagScanned.status': 'updating'}]});
+		users.forEach(function(user) {
+			console.log(user.gamertagScanned);
+			leaderboardsApi.buildUserRanks(user._id);
+			leaderboardsApi.updateUserCounts(user._id);
+		});
+	}
+});
+
+SyncedCron.add({
+	name: "optomistic update user leaderboard counts",
+	schedule: function(parser) {
+		return parser.text('every 20 min');
+	},
+	job: function() {
+		var onlineUsers = meteor.users.find({"status.online": true});
+		onlineUsers.forEach(function(user) {
+			if (!user.gamertagScanned || user.gamertagScanned.status === 'false' || user.gamertagScanned.status === 'building') {
+				return;
+			}
+
+			//the buildUserRanks function will return and do nothing if the user already has a leaderboard doc. 
+			leaderboardsApi.buildUserRanks(user._id);
+			leaderboardsApi.updateUserCounts(user._id);
+		});
+	}
+});
