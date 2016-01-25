0008ea5398e26688373251cc6102b4f2b814df4b
kg187 <kguirao87@gmail.com>
2016-01-25 16:54:01 -0500

    resolving conflicts

--- server/auth/user_cron.js
+++ server/auth/user_cron.js
@@ -1,7 +1,7 @@
 // SyncedCron.add({
 // 	name: "update all user stats",
 // 	schedule: function(parser) {
-// 		return parser.text('every 2 hours');
+// 		return parser.text('every 1 hours');
 // 	},
 // 	job: function() {
 // 		var users = Meteor.users.find({ $or: [{ 'gamertagScanned.status': 'true' }, { 'gamertagScanned.status': 'updating' }] });
