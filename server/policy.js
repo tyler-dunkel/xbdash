769f666f93b7ce69ae9d66509514f1c496e2948f
kg187 <kguirao87@gmail.com>
2016-04-20 23:06:01 -0400

    indent fixes

--- server/policy.js
+++ server/policy.js
@@ -5,33 +5,33 @@ BrowserPolicy.content.allowInlineStyles();
 BrowserPolicy.content.allowFontDataUrl();
 
 var trusted = [
-  '*.bootstrapcdn.com',
-  '*.facebook.com',
-  '*.facebook.net',
-  '*.google.com',
-  '*.google-analytics.com',
-  '*.mxpnl.com',
-  '*.polygon.com',
-  '*.ritetag.com',
-  '*.soundcloud.com',
-  '*.twitter.com',
-  '*.vox-cdn.com',
-  '*.voxmedia.com',
-  '*.xbox.com',
-  '*.xboxlive.com',
-  '*.youtube.com',
-  '*.ritetag.com',
-  '*.cloudinary.com',
-  '*.xbdash.com'
+	'*.bootstrapcdn.com',
+	'*.facebook.com',
+	'*.facebook.net',
+	'*.google.com',
+	'*.google-analytics.com',
+	'*.mxpnl.com',
+	'*.polygon.com',
+	'*.ritetag.com',
+	'*.soundcloud.com',
+	'*.twitter.com',
+	'*.vox-cdn.com',
+	'*.voxmedia.com',
+	'*.xbox.com',
+	'*.xboxlive.com',
+	'*.youtube.com',
+	'*.ritetag.com',
+	'*.cloudinary.com',
+	'*.xbdash.com'
 ];
 
 _.each(trusted, function(origin) {
-  originSecure = "https://" + origin;
-  origin = "http://" + origin;
-  BrowserPolicy.content.allowImageOrigin(originSecure);
-  BrowserPolicy.content.allowImageOrigin(origin);
-  BrowserPolicy.content.allowOriginForAll(originSecure);
-  BrowserPolicy.content.allowOriginForAll(origin);
-  BrowserPolicy.content.allowFrameOrigin(originSecure);
-  BrowserPolicy.content.allowFrameOrigin(origin);
+	originSecure = "https://" + origin;
+	origin = "http://" + origin;
+	BrowserPolicy.content.allowImageOrigin(originSecure);
+	BrowserPolicy.content.allowImageOrigin(origin);
+	BrowserPolicy.content.allowOriginForAll(originSecure);
+	BrowserPolicy.content.allowOriginForAll(origin);
+	BrowserPolicy.content.allowFrameOrigin(originSecure);
+	BrowserPolicy.content.allowFrameOrigin(origin);
 });
