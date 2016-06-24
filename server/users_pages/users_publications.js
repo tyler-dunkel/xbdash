7815421c5f948331a24d682721d3613499ea8981
kg187 <kguirao87@gmail.com>
2016-06-24 11:42:02 -0400

    commented out clips

--- server/users_pages/users_publications.js
+++ server/users_pages/users_publications.js
@@ -156,25 +156,22 @@ Meteor.publishComposite('userClips', function(gamertagSlug) {
 				find: function(user) {
 					var currentTime = moment().format();
 					return gameClips.find({ 
-							userId: user._id,
-							gameClipUris: {
-								$elemMatch: {
-									uriType: "Download",
-									expiration: {
-										$gte: currentTime
-									}
-								}
+							"userId": user._id,
+							"gameClipUris.uriType": {
+								$in: [
+									"Download"
+								]
 							}
 						}, {
-						sort: { "gameClipUris.0.expiration": -1 },
+						sort: { "datePublished": -1 },
 						fields: {
+							"datePublished": -1,
 							"userId": 1,
 							"titleId": 1,
 							"titleName": 1,
 							"thumbnails": 1,
 							"gameClipUris": 1
 						},
-						sort: { "datePublished": -1 },
 						limit: 6
 					});
 				}
