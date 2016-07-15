4408a72f80bd3c976b3ba13f37072b519a2ca8a8
kg187 <kguirao87@gmail.com>
2016-07-15 02:16:26 -0400

    Revert "fixes for non-user"
    This reverts commit 35f1576820f4ddb6fb17a346397f85a06a0e6397.

--- client/templates/games_pages/games_single_page.js
+++ client/templates/games_pages/games_single_page.js
@@ -3,13 +3,10 @@ Template.gamesSinglePageNew.created = function() {
 	DocHead.removeDocHeadAddedTags();
 	var self = this,
 		slug = Router.current().params.slug,
-		gamertagSlug;
-	if (Meteor.user()) {
 		gamertagSlug = Meteor.user().gamertagSlug;
-		this.subscribe('userWishlist', gamertagSlug);
-		this.subscribe('userTrophyCase', gamertagSlug);
-	}
 	this.subscribe('singleGame', slug);
+	this.subscribe('userWishlist', gamertagSlug);
+	this.subscribe('userTrophyCase', gamertagSlug);
 }
 
 Template.gamesSinglePageNew.helpers({
@@ -72,11 +69,9 @@ Template.gamesSinglePageNew.helpers({
 		return false; 
 	},
 	chkUserWishlist: function() {
-		if (Meteor.user()) {
-			var wishlistCount = userWishlists.find({ userId: Meteor.userId(), relationId: this.gameId }).count();
-			if (wishlistCount > 0) {
-				return true;
-			}
+		var wishlistCount = userWishlists.find({ userId: Meteor.userId(), relationId: this.gameId }).count();
+		if (wishlistCount > 0) {
+			return true;
 		}
 	},
 	chkIfCompleted: function () {
