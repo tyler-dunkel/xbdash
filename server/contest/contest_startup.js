40ca516b7090eb1848f3854551fef8a293e92e50
Tyler Dunkel <Tyler.Dunkel@morningstar.com>
2016-07-25 20:12:05 -0500

    fix

--- server/contest/contest_startup.js
+++ server/contest/contest_startup.js
@@ -2,7 +2,7 @@ Meteor.startup(function() {
 
 	Meteor.setInterval(function() {
 		xbdContests.find({status: 'waiting'}).forEach(function(contest) {
-			if (contest.startDate < new Date()) {
+			if (contest.startDate < moment().utc().toDate()) {
 				xbdContests.update({_id: contest._id}, {$set: {status: 'active'}});
 			}
 		});
@@ -11,7 +11,7 @@ Meteor.startup(function() {
 	//check for active contests that need to end. if ending, set to choosing winner and create a job to do a scan. 
 	Meteor.setInterval(function() {
 		xbdContests.find({status: 'active'}).forEach(function(contest) {
-			if (contest.endDate > new Date()) {
+			if (contest.endDate > moment().utc().toDate()) {
 				xbdContests.update({_id: contest._id}, {$set: {status: 'choosing-winner'}});
 				userContestEntries.update({contestToken: contest.contestToken}, {$set: {status: 'choosing-winner'}});
 				var chooseContestWinner = new Job(xbdJobsCollection, 'chooseContestWinner', { contestToken: contest.contestToken })
