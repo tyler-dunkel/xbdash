a034ab1972ff613e341cc82c9436a3d864239cf7
kg187 <kguirao87@gmail.com>
2016-07-29 00:19:50 -0400

    working on ad integration

--- client/templates/ads/ads.js
+++ client/templates/ads/ads.js
@@ -1,17 +1,35 @@
-Template.googleAdsMedium.onRendered = function() {
-	$.getScript("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", function() {
+Template.googleAdsMedium.rendered = function() {
+	$.getScript("//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", function() {
 		var ads, adsbygoogle;
-		ads = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2014481178377974" data-ad-slot="9677223644" data-ad-format="auto"></ins>';
-		$('.responsive-top-banner').html(ads);
-		return (adsbygoogle = window.adsbygoogle || []).push({});
+		ads = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle xbd-top-banner-ad" style="display: block;" data-ad-client="ca-pub-2014481178377974" data-ad-slot="9677223644" data-ad-format="auto"></ins>';
+		$('.xbd-top-banner-ad').html(ads);
+		adsbygoogle = window.adsbygoogle || [];
+		window.onload = function() {
+			return (adsbygoogle).push({});
+		}
 	});
 };
 
-Template.googleAdsResponsiveRight.onRendered = function() {
-	$.getScript("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", function() {
+Template.googleAdsResponsiveRight.rendered = function() {
+	$.getScript("//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", function() {
 		var ads, adsbygoogle;
-		ads = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2014481178377974 data-ad-slot="5107423240" data-ad-format="auto"></ins>';
-		$('.responsive-right-banner').html(ads);
-		return (adsbygoogle = window.adsbygoogle || []).push({});
+		ads = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle xbd-right-banner-ad" style="display: block;" data-ad-client="ca-pub-2014481178377974" data-ad-slot="5107423240" data-ad-format="auto"></ins>';
+		$('.xbd-right-banner-ad').html(ads);
+		adsbygoogle = window.adsbygoogle || [];
+		window.onload = function() {
+			return (adsbygoogle).push({});
+		}
 	});
 };
+
+// Template.googleAdsMedium.helpers({
+// 	'unique': function() {
+// 		return (new Date()).valueOf();
+// 	}
+// });
+
+// Template.googleAdsResponsiveRight.helpers({
+// 	'unique': function() {
+// 		return (new Date()).valueOf();	
+// 	}
+// });
