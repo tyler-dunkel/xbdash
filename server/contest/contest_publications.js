cec793f8bd0f2d4b3cf632e9aad4b27eb5f6ee79
Tyler Dunkel <Tyler.Dunkel@morningstar.com>
2016-08-13 13:17:27 -0500

    fix

--- server/contest/contest_publications.js
+++ server/contest/contest_publications.js
@@ -7,25 +7,27 @@ Meteor.publishComposite('xbdContestsPub', function() {
 			{
 				find: function(contest) {
 					var user = Meteor.users.findOne({ _id: this.userId });
-					var contestEntry = userContestEntries.findOne({ status: 'active', contestToken: contest.contestToken, userId: this.userId });
-					return Meteor.users.find({ _id: contestEntry.userId }, {
-						fields: {
-							"gamercard.gamertag": 1,
-							"gamercard.gamerscore": 1,
-							"gamercard.gamerpicLargeSslImagePath": 1,
-							"gamertagSlug": 1
-						}
-					});
-				},
-				find: function(contest) {
-					var user = Meteor.users.findOne({ _id: this.userId });
 					if (user) {
 						return userContestEntries.findOne({ status: 'active', contestToken: contest.contestToken, userId: this.userId });
 					}
 				},
 				find: function(contest) {
 					return userContestEntries.find({ status: 'active', contestToken: contest.contestToken }, { sort: { 'data.value': -1 }, limit: 10 });
-				}
+				},
+				children: [
+					{
+						find: function(contest, entry) {
+							return Meteor.users.find({ _id: entry.userId }, {
+								fields: {
+									"gamercard.gamertag": 1,
+									"gamercard.gamerscore": 1,
+									"gamercard.gamerpicLargeSslImagePath": 1,
+									"gamertagSlug": 1
+								}
+							});
+						}
+					}
+				]
 			}
 		]
 	}
