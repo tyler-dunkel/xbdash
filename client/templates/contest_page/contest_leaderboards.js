063f345dc54e333434be82dd25703fe2ff06d603
kg187 <kguirao87@gmail.com>
2016-08-13 21:13:03 -0400

    template fix

--- client/templates/contest_page/contest_leaderboards.js
+++ client/templates/contest_page/contest_leaderboards.js
@@ -33,7 +33,7 @@ Template.contestLeaderboardTemplate.helpers({
 	getUserImage: function() {
 		var user = Meteor.users.findOne({ _id: this.userId });
 		var defaultGamerImage = '/img/gamerpic-default.jpg';
-		if (user && user.gamercard && user.gamercard.gamerpicLargeSslImagePath) {
+		if (user && user.gamercard && user.gamercard.gamerpicLargeSslImagePath && (user.gamercard.gamerpicLargeSslImagePath !== '')) {
 			defaultGamerImage = "https://res.cloudinary.com/xbdash/image/fetch/c_fit,w_64,h_64/" + encodeURIComponent(user.gamercard.gamerpicLargeSslImagePath);
 		}
 		return defaultGamerImage;
